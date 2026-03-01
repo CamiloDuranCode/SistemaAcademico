5d41c82b81547c4defeadc56de8d8f0ac745badb
Camilo Duran <camiloandresduran@unicesar.edu.co>
2026-03-01 12:56:56 -0500

    feat: implementar submenu para estudiante

--- src/sistemaacademico/Main.java
+++ src/sistemaacademico/Main.java
@@ -55,7 +55,26 @@ public class Main {
     }
     
     public static void menuEstudiantes() {
-        
+        int opcion;
+
+        do {
+            System.out.println("\n--- GESTIÓN DE ESTUDIANTES ---");
+            System.out.println("1. Registrar");
+            System.out.println("2. Listar");
+            System.out.println("3. Actualizar");
+            System.out.println("4. Eliminar");
+            System.out.println("5. Volver");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1: registrarEstudiante(); break;
+                case 2: listarEstudiantes(); break;
+                case 3: actualizarEstudiante(); break;
+                case 4: eliminarEstudiante(); break;
+            }
+
+        } while (opcion != 5);
     }
     
     public static void menuAsignaturas() {
