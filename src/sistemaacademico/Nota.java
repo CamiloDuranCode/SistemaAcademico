e3c6f0346e84840b17eadcb14e64cd1d412d1932
Juan Rangel <jcamilorangel@unicesar.edu.co>
2026-03-01 12:57:26 -0500

    feat: crear clase Nota con atributos

---
+++ src/sistemaacademico/Nota.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author JUAN CAMILO RANGEL
+ */
+public class Nota {
+    
+    Estudiante estudiante;
+    Asignatura asignatura;
+    double valor;
+    String periodo;
+    
+}
