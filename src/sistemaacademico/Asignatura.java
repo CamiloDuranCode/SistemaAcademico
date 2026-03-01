5450162f5c75378e64ec96ddbe6fe9a0c32a930a
Juan Rangel <jcamilorangel@unicesar.edu.co>
2026-03-01 12:36:48 -0500

    feat: crear clase Asignatura con atributos

---
+++ src/sistemaacademico/Asignatura.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+
+public class Asignatura {
+    
+    String codigo;
+    String nombre;
+    int creditos;
+    String docente;
+    
+}
