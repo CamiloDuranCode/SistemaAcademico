a2d647d4af833f885d0785a34af877ea3459bc71
WILBERTO ARIZA ZAPATA <warizaz@unicesar.edu.co>
2026-03-01 14:14:56 -0500

    feat: implementar CREATE - registrar asignatura

--- src/sistemaacademico/Main.java
+++ src/sistemaacademico/Main.java
@@ -4,10 +4,53 @@
  */
 package sistemaacademico;
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author camil
  */
 public class Main {
+static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+static Scanner sc = new Scanner(System.in);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void registrarAsignatura() {
+
+    System.out.println("\n    REGISTRAR ASIGNATURA    ");
+
+    System.out.print("Codigo: ");
+    String codigo = sc.nextLine();
+
+    System.out.print("Nombre: ");
+    String nombre = sc.nextLine();
+
+    System.out.print("Creditos: ");
+    int creditos = sc.nextInt();
+    sc.nextLine(); 
+
+    System.out.print("Docente: ");
+    String docente = sc.nextLine();
+
+    Asignatura nueva = new Asignatura(codigo, nombre, creditos, docente);
+
+    asignaturas.add(nueva);
+
+    System.out.println("Asignatura registrada correctamente.");
+}
     
 }
